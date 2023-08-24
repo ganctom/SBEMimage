d8f7dc73780306b7cdaf990859e7c68c173eb715
Ganci <gancarcik.tomas@gmail.com>
2023-08-24 14:33:22 +0200

    AFSS logging improved

--- src/acquisition.py
+++ src/acquisition.py
@@ -1278,12 +1278,12 @@ class Acquisition:
                 if rej_fits:
                     msg = f'Reliable results could not be found for following tiles:'
                     utils.log_info('CTRL', msg)
-                    self.add_to_main_log('CTRL: ' + msg)
+                    self.add_to_main_log(f'CTRL: ' + msg)
                     self.add_to_afss_log(msg)
                     for val in rej_fits.values():
                         msg = val[1]
                         utils.log_info('CTRL', msg)
-                        self.add_to_main_log('CTRL: ' + msg)
+                        self.add_to_main_log(f'CTRL: ' + msg)
                         self.add_to_afss_log(msg)
 
                 if diffs_passed and nr_good_fits not in [-1, 0]:
@@ -1294,20 +1294,20 @@ class Acquisition:
                         s = 'outlier' if nr_of_outliers == 1 else 'outliers'
                         msg = f'Discarding {nr_of_outliers} {s} from averaging.'
                         utils.log_info('CTRL', msg)
-                        self.add_to_main_log('CTRL: ' + msg)
+                        self.add_to_main_log(f'CTRL: ' + msg)
                         self.add_to_afss_log(msg)
 
                     # Log info about results of either Focus or Stigmator series
                     msg = f'Amount of failed/over RMSE limit/filtered fits: {n_fail}/{n_lim}/{n_outs}'
                     utils.log_info('CTRL', msg)
-                    self.add_to_main_log('CTRL: ' + msg)
+                    self.add_to_main_log(f'CTRL: ' + msg)
                     self.add_to_afss_log(msg)
 
                     if self.autofocus.afss_consensus_mode == 1 \
                             or (self.autofocus.afss_consensus_mode == 2 and mode == 'focus'):
                         msg = f'Applying corrections to all tracked tiles:'
                         utils.log_info('CTRL', msg)
-                        self.add_to_main_log('CTRL: ' + msg)
+                        self.add_to_main_log(f'CTRL: ' + msg)
                         self.add_to_afss_log(msg)
                         for msg in log_msgs.values():
                             self.add_to_main_log(msg)
@@ -1322,7 +1322,7 @@ class Acquisition:
                         msg = ' '.join(['Applying average', f'{dx[mode][0]}', 'correction', f'{dx[mode][1]}',
                                         'to all tracked tiles.'])
                         utils.log_info('CTRL', msg)
-                        self.add_to_main_log('CTRL: ' + msg)
+                        self.add_to_main_log(f'CTRL: ' + msg)
                         self.add_to_afss_log(msg)
 
                     #   Reset fail counter of current afss mode if AFSS run was successful
@@ -1339,7 +1339,7 @@ class Acquisition:
                         msg_2 = f'Resetting original {d[self.autofocus.afss_mode]} values.'
                         for msg in [msg_0, msg_1, msg_2]:
                             utils.log_info('CTRL', msg)
-                            self.add_to_main_log('CTRL: ' + msg)
+                            self.add_to_main_log(f'CTRL: ' + msg)
                             self.add_to_afss_log(msg)
                             self.autofocus.afss_set_orig_wd_stig()
                     # No reliable fit was found
@@ -1347,7 +1347,7 @@ class Acquisition:
                         msg = f'Interpolation of all tracked tiles failed. ' \
                               f'Resetting original {d[self.autofocus.afss_mode]} values.'
                         utils.log_info('CTRL', msg)
-                        self.add_to_main_log('CTRL: ' + msg)
+                        self.add_to_main_log(f'CTRL: ' + msg)
                         self.add_to_afss_log(msg)
                         self.autofocus.afss_set_orig_wd_stig()
                     # Correction not within user defined permitted range
@@ -1360,7 +1360,7 @@ class Acquisition:
                             msg_2 = f'Resetting original {d[self.autofocus.afss_mode]} values.'
                             for msg in [msg_0, msg_1, msg_2]:
                                 utils.log_info('CTRL', msg)
-                                self.add_to_main_log('CTRL: ' + msg)
+                                self.add_to_main_log(f'CTRL: ' + msg)
                                 self.add_to_afss_log(msg)
                                 self.autofocus.afss_set_orig_wd_stig()
                         else:
@@ -1372,12 +1372,12 @@ class Acquisition:
                             msg = f'{d[self.autofocus.afss_mode]} corrections of following tiles discarded ' \
                                   f'(out of permitted range):'
                             utils.log_info('CTRL', msg)
-                            self.add_to_main_log('CTRL: ' + msg)
+                            self.add_to_main_log(f'CTRL: ' + msg)
                             self.add_to_afss_log(msg)
                             # Reset corrections that are out of permitted range and ensure that orig values are reset
                             for tile_key, val in rej_thr.items():
                                 utils.log_info('CTRL', val[1])
-                                self.add_to_main_log('CTRL: ' + val[1])
+                                self.add_to_main_log(f'CTRL: ' + val[1])
                                 self.add_to_afss_log(val[1])
                                 grid_index, tile_index = map(int, str.split(tile_key, '.'))
                                 orig_wd = rejected_tiles[tile_key][2][0][0]
